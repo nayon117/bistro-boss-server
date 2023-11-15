97b9d59f9973273c59bb2122d57e5102c0693251
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-15 07:31:29 +0600

    create get method for carts

--- index.js
+++ index.js
@@ -28,12 +28,33 @@ async function run() {
       await client.connect();
       
       const menuCollection = client.db('bistroDB').collection('menu')
+      const reviewCollection = client.db('bistroDB').collection('reviews')
+      const cartCollection = client.db('bistroDB').collection('carts')
 
       app.get('/menu', async (req, res) => {
           const result = await menuCollection.find().toArray()
           res.send(result)
       })
+      app.get('/reviews', async (req, res) => {
+          const result = await reviewCollection.find().toArray()
+          res.send(result)
+      })
 
+    // carts
+
+    // get 
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = {email:email}
+      const result = await cartCollection.find(query).toArray()
+      res.send(result)
+    })
+    
+    app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem)
+      res.send(result)
+    })
 
 
 
