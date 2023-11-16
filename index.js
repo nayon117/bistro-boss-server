26bba6753f05d1c46b6fbe7aba101640a4a2e84c
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-16 19:54:10 +0600

    implement jwt and verify token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -31,16 +32,59 @@ async function run() {
     const reviewCollection = client.db("bistroDB").collection("reviews");
     const cartCollection = client.db("bistroDB").collection("carts");
 
+
+    // jwt related api ---------------------------
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET_TOKEN, { expiresIn: '1h' })
+      res.send({token})
+    })
+
+
+    // middlewares for verify token
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({message:'forbidden access'})
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({message:'forbidden access'})
+        }
+        req.decoded = decoded;
+        next()
+      })
+    }
+
+
+
+
     // --------- user collection -----------------
 
-    app.get("/users", async (req, res) => {
+    app.get("/users",verifyToken, async (req, res) => {
       try {
+       
         const result = await userCollection.find().toArray();
         res.send(result);
       } catch (error) {
         console.log(error);
       }
     });
+
+    app.get('/users/admin/:email',verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send({message:'unauthorized access'})
+      }
+      const query = {email:email}
+      const user = await userCollection.findOne(query)
+      let admin = false;
+      if (user) {
+        admin = user?.role==='admin'
+      }
+      res.send({admin})
+    })
 
     app.post("/users", async (req, res) => {
       const user = req.body;
