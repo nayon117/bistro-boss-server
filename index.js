9b4e7da5948ff5f90592e95226012c8b13605d98
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-21 11:09:18 +0600

    create admin-stats

--- index.js
+++ index.js
@@ -221,37 +221,63 @@ async function run() {
       });
     });
 
-
-    app.get('/payments/:email', verifyToken, async (req, res) => {
-      const query = { email: req.params.email }
+    app.get("/payments/:email", verifyToken, async (req, res) => {
+      const query = { email: req.params.email };
       if (req.params.email !== req.decoded.email) {
-        return res.status(403).send({message:'forbidden access'})
+        return res.status(403).send({ message: "forbidden access" });
       }
-      const result = await paymentCollection.find(query).toArray()
-      res.send(result)
-    })
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
 
-
-    app.post('/payments', async (req, res) => {
+    app.post("/payments", async (req, res) => {
       const payment = req.body;
-     
-      const paymentResult = await paymentCollection.insertOne(payment)
+
+      const paymentResult = await paymentCollection.insertOne(payment);
 
       // delete each item from cart
       const query = {
         _id: {
-        $in:payment.cartIds.map(id=> new ObjectId(id))
-        }
-      }
-      const deleteResult = await cartCollection.deleteMany(query)
+          $in: payment.cartIds.map((id) => new ObjectId(id)),
+        },
+      };
+      const deleteResult = await cartCollection.deleteMany(query);
 
+      res.send({ paymentResult, deleteResult });
+    });
 
-      res.send({paymentResult,deleteResult})
-    })
+    // stats or analytics
+    app.get("/admin-stats", verifyToken,verifyAdmin, async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+      // const payments = await paymentCollection.find().toArray();
+      // const revenue = payments.reduce(
+      //   (total, payment) => total + payment.price,
+      //   0
+      // );
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$price",
+              },
+            },
+          },
+        ])
+        .toArray();
 
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
 
-
-
+      res.send({
+        users,
+        menuItems,
+        orders,
+        revenue,
+      });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
